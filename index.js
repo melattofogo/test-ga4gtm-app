8c2db6ace54b447cc5fe1935762d04014d2adaaa
Fogo <melatto.fogo@live.com>
2023-05-18 11:08:24 -0300

    feat(index.js): adiciona renderizacao de index na raiz e adiciona rota submit

--- index.js
+++ index.js
@@ -1,9 +1,74 @@
+const { log } = require('console');
 const express = require('express');
+var bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
+
+//support json-encoded bodies
+app.use( bodyParser.json() );
+//support url-encoded bodies
+app.use( bodyParser.urlencoded({
+    extended: true
+}));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// Serve static files from the "public" directory
+app.use('/public', express.static(path.join(__dirname,'public') ));
+
+app.set('/views', path.join(__dirname, 'views'));
+
+var formInput = [
+    {
+        for: 'name',
+        label: 'Name',
+        tag: 'input',
+        type: 'text',
+        id: 'name',
+        name: 'name',
+        required: true
+    },
+    {
+        for: 'email',
+        label: 'Email',
+        tag: 'input',
+        type: 'email',
+        id: 'email',
+        name: 'email',
+        required: true
+    },
+    {
+        for: 'message',
+        label: 'Message',
+        tag: 'textarea',
+        type: 'message',
+        id: 'message',
+        name: 'message',
+        rows: 4,
+        required: true
+    }
+];
 
 // Define a route for the homepage
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+
+    res.render('index', {formInput});
+
+});
+
+app.post('/submit', (req, res) => {
+
+    var formRequest = req.body;
+
+    log('/submit');
+    log(formRequest);
+
+    var keys = Object.keys(formRequest);
+    var values = Object.values(formRequest);
+
+    res.render('submit',{keys, values});
 });
 
 // Start the server
